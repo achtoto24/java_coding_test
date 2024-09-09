2236f43b93c554b8ba2378df7ff5c4da403f53be
Jeo Ahn <ach_toto24@naver.com>
2024-09-09 19:34:08 +0900

    first commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        String str = "123";
+        
+        int num = Integer.parseInt(str);
+
+        //System.out.println(num);
+        System.out.println(str.length());
+    }
+}
