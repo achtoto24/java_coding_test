6c8b52f22321540f2081b5f8e230f6dcc6cdadf0
achtoto <ach_toto24@naver.com>
2024-09-29 17:17:12 +0900

    try-12

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner stdln = new Scanner(System.in);
-        int subject = stdln.nextInt();
-        int[] arr = new int[subject];
+        int A = stdln.nextInt();
+        int B = stdln.nextInt();
+        int V = stdln.nextInt();
 
-        for (int i = 0; i < subject; i++) {
-            int score = stdln.nextInt();
-            arr[i] = score;
-        }
-
-        int max = arr[0];
-        for (int i = 1; i < subject; i++) {
-            if (max < arr[i])
-                max = arr[i];
-        }
-
-        float sum = 0f;
-        for (float i : arr) {
-            i = (i / max) * 100;
-            sum += i;
-        }
+        int day = (V - B) / (A - B);    // A - B를 한 세트로 생각하기, V까지 올라가기만 하면 되므로 B를 제외시키기
         
-        System.out.println(sum / subject);
+        if  (((V - B) % (A - B)) != 0)  // 총 길이가 세트로 나누어 떨어지지 않을 경우 하루를 더 추가해서 올라가야 함
+            day++;
+
+        System.out.println(day);
+
     }
 }
