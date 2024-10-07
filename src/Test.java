cb9e48ab85d2a507e739193a04dde4ea647564ef
Jeo Ahn <ach_toto24@naver.com>
2024-10-07 23:01:37 +0900

    2024.10.07

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,20 @@
-
+import java.util.Scanner;
+ 
 public class Test {
-    public static void main(String[] args) {
-        //Scanner stdln = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
         
-        System.out.println("what?");
-    }
+		int N = in.nextInt();
+ 
+		for (int i = 2; i <= Math.sqrt(N); i++) {	// 또는 i * i <= N
+			while (N % i == 0) {
+				System.out.println(i);
+				N /= i;
+			}
+		}
+
+		if (N != 1)     // N % i == 0을 만족시키지 못 했을 경우 최종 N은 출력을 하지 못하는 것을 방지
+			System.out.println(N);
+		
+	}
 }
