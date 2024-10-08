438ab85992c264d8747f0fda2d4e2448be5864d5
Jeo Ahn <ach_toto24@naver.com>
2024-10-08 23:13:27 +0900

    2024.10.08

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 		Scanner stdln = new Scanner(System.in);
-        int x = stdln.nextInt();
-        int y = stdln.nextInt();
-        int w = stdln.nextInt();
-        int h = stdln.nextInt();
-        int[] arr = new int[4];
+        int len = stdln.nextInt();
 
-        arr[0] = x;
-        arr[1] = w - x;
-        arr[2] = y;
-        arr[3] = h - y;
-
-        int min = arr[0];
-        for (int i = 1; i < 4; i++) {
-            if (arr[i] < min)
-                min = arr[i];
-        }
-
-        System.out.println(min);
+        System.out.println(len + len * 3);
     }
 }
