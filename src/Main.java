3571ae4e1dc6b433c0c095a5c45155ad9d13565d
Jeo Ahn <ach_toto24@naver.com>
2024-10-11 22:54:22 +0900

    2024.10.11

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,20 @@
 import java.util.Scanner;
- 
-public class Main {
+
+public class Main{
+public static void main(String[] args){
     
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int side1 = sc.nextInt();
-        int side2 = sc.nextInt();
-        int side3 = sc.nextInt();
-        
-        // 가장 긴 변의 길이 구하기
-        int maxSide = Math.max(Math.max(side1, side2), side3);
-        
-        // 삼각형 조건을 만족하는지 확인
-        if (maxSide < side1 + side2 + side3 - maxSide) {
-            System.out.println(side1 + side2 + side3);
-            }
-        else {
-            int sum = side1 + side2 + side3 - maxSide;
-            System.out.println(2 * sum - 1);
-        }
-        
+    Scanner sc = new Scanner(System.in);
+
+    int a1 = sc.nextInt();
+    int a0 = sc.nextInt();
+    int c = sc.nextInt();
+    int n0 = sc.nextInt();
+
+    if((a1*n0+a0)<= c*n0 && a1 <= c){   // a1 <= c는 음수가 아닌 조건을 의미함
+        System.out.println(1);
+    }else{
+        System.out.println(0);
     }
-    
+    sc.close();
+    }
 }
-
-
