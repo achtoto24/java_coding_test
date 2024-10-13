e9fef9290aa859d43894333abe4387c29f5d8669
Jeo Ahn <ach_toto24@naver.com>
2024-10-13 22:39:10 +0900

    2024.10.13

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,22 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-    
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        String N_str = String.valueOf(N);
-        int M = 0;
-        
-        for (int i = N - 9 * N_str.length(); i <= N - N_str.length(); i++) {
-            String str = String.valueOf(i);
-            
-            int sum = i;
-            for (int j = 0; j < str.length(); j++) 
-                sum += Integer.valueOf(str.charAt(j) - '0');
-            
-            if (sum == N) {
-                M = i;
-                break;
+        int order = sc.nextInt();
+        int num = 666;
+        int cnt = 0;
+
+        while (true) {
+            String num_str = String.valueOf(num);
+            if (num_str.contains("666")) {
+                if (++cnt == order) {
+                    System.out.println(num);
+                    break;
+                }
             }
+            num++;
         }
 
-        System.out.println(M);
+
     }
 }
