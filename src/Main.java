7fa27352dba287b37a57925a8e7434d586c0117d
Jeo Ahn <ach_toto24@naver.com>
2024-10-14 23:16:15 +0900

    2024.10.14

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,57 @@
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int order = sc.nextInt();
-        int num = 666;
-        int cnt = 0;
+public class Main {
+    public static boolean[][] arr;
+    public static int min = 64;
 
-        while (true) {
-            String num_str = String.valueOf(num);
-            if (num_str.contains("666")) {
-                if (++cnt == order) {
-                    System.out.println(num);
-                    break;
-                }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner((System.in));
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        arr = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String str = sc.next();
+
+            for (int j = 0; j < M; j++) {
+                if (str.charAt(j) == 'W')
+                    arr[i][j] = true;   // 'W'일 때 true
+                else
+                    arr[i][j] = false;  // 'B'일 때 false
             }
-            num++;
         }
 
+        // (i, j)는 8x8 체스판의 시작 지점
+        for (int i = 0; i < N - 7; i++) {
+            for (int j = 0; j < M - 7; j++) {
+                find(i, j);
+            }
+        }
+
+        System.out.println(min);
 
     }
+
+    static void find(int x, int y) {
+        boolean tf = arr[x][y]; // 기준 칸 값
+        int count = 0;
+        
+        for (int i = x; i < x + 8; i++) {
+            for (int j = y; j < y + 8; j++) {
+                if (arr[i][j] != tf)
+                    count++;
+                    
+                tf = !(tf);     // 다음 칸은 현재(기준) 칸 값과 반대
+            }
+            tf = !(tf); // 다음 줄 첫 번째 칸은 현재 줄 첫 번째 칸 값과 반대
+        }
+
+        int min_count = Math.min(count, 64 - count);    // 첫 번째 칸이 'W'이거나 'B' 두가지 경우 비교 
+
+        min = Math.min(min, min_count);
+    }
+
 }
--- src/Test.java
+++ src/Test.java
@@ -1,49 +1,46 @@
 import java.util.Scanner;
 
 public class Test {
-
     public static void main(String[] args) {
-		Scanner stdln = new Scanner(System.in);
-        int cnt = stdln.nextInt();
-        int[] xp = new int[cnt];
-        int[] yp = new int[cnt];
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        sc.nextLine(); // 다음 줄바꿈을 소비합니다.
 
-        for (int i = 0; i < cnt; i++) {
-            int x = stdln.nextInt();
-            xp[i] = x;
-            int y = stdln.nextInt();
-            yp[i] = y;
+        char[][] arr = new char[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String str = sc.nextLine();
+            for (int j = 0; j < M; j++) 
+                arr[i][j] = str.charAt(j);
         }
 
-        
-        if (cnt == 1) {
-            System.out.println(0);
-            return;
-        }
-        
-        int max1 = xp[0];
-        int min1 = xp[0];
-        
-        for (int i = 1; i < cnt; i++) {
-            if (xp[i] > max1)
-            max1 = xp[i];
-            if (xp[i] < min1)
-            min1 = xp[i];
-        }
-        
-        int max2 = yp[0];
-        int min2 = yp[0];
+        int min = Integer.MAX_VALUE;
 
-        for (int i = 1; i < cnt; i++) {
-            if (yp[i] > max2)
-                max2 = yp[i];
-            if (yp[i] < min2)
-                min2 = yp[i];
+        // 체스판 패턴을 비교
+        for (int i = 0; i <= N - 8; i++) {
+            for (int j = 0; j <= M - 8; j++) {
+                int count1 = 0; // 'B'로 시작하는 경우
+                int count2 = 0; // 'W'로 시작하는 경우
+
+                for (int k = 0; k < 8; k++) {
+                    for (int l = 0; l < 8; l++) {
+                        char expectedChar1 = (k + l) % 2 == 0 ? 'B' : 'W';
+                        char expectedChar2 = (k + l) % 2 == 0 ? 'W' : 'B';
+
+                        if (arr[i + k][j + l] != expectedChar1) {
+                            count1++;
+                        }
+                        if (arr[i + k][j + l] != expectedChar2) {
+                            count2++;
+                        }
+                    }
+                }
+                min = Math.min(min, Math.min(count1, count2));
+            }
         }
 
-        if (max1 == min1 || max2 == min2)
-            System.out.println(0);
-        else
-            System.out.println((max1 - min1) * (max2 - min2));
+        System.out.println(min);
+        sc.close(); // Scanner를 닫습니다.
     }
 }
