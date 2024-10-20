2a94a9096d524784a057d5126bfa1a344703532a
Jeo Ahn <ach_toto24@naver.com>
2024-10-20 23:22:18 +0900

    2024.10.20

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,38 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        String[] str = new String[N];
         
-        StringBuffer sb = new StringBuffer();
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        String[][] arr = new String[N][2];
+
+        sc.nextLine(); // 개행 버림
 
         for (int i = 0; i < N; i++) {
-            str[i] = sc.next();
+            arr[i][0] = sc.next();
+            arr[i][1] = sc.next();
         }
 
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (str[i].length() > str[j].length()) {
-                    String temp = str[i];
-                    str[i] = str[j];
-                    str[j] = temp;
-                }
+        Arrays.sort(arr, new Comparator<String[]>() {
+            public int compare(String[] s1, String[] s2) {
+
+                int num1 = Integer.parseInt(s1[0]);
+                int num2 = Integer.parseInt(s2[0]);
+
+                if (num1 != num2) 
+                    return Integer.compare(num1, num2);
+                else
+                    return 0;
+
             }
-        }
+        });
 
+        for (int i = 0; i < N; i++) 
+            System.out.println(arr[i][0] + " " + arr[i][1]);
         
-
-        for (String i : str)
-            System.out.println(i);
-
-        
-
     }
 }
