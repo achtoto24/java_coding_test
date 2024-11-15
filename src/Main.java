271837b4202f892c469bfb2c7beabe4957272d98
Jeo Ahn <ach_toto24@naver.com>
2024-11-15 22:59:12 +0900

    2024.11.15

--- src/Main.java
+++ src/Main.java
@@ -2,55 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int num = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        int[] arr = new int[num];
-        for (int i = 0; i < num; i++)
-            arr[i] = Integer.parseInt(br.readLine());
-        
-        Stack<Double> stack = new Stack<>();
-        HashMap<Character, Integer> map = new HashMap<>();
+
+        Stack<Character> stack = new Stack<>();
 
         int cnt = 0;
-        for (int i = 0; i < str.length(); i++) {
-            char currentChar = str.charAt(i);
+        int stack_len = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            char ch = str.charAt(i);
 
-            if (currentChar == '+') {
-                double n1 = stack.pop();
-                double n2 = stack.pop();
-                double rst = n2 + n1;
-                stack.push(rst);
-            } else if (currentChar == '-') {
-                double n1 = stack.pop();
-                double n2 = stack.pop();
-                double rst = n2 - n1;
-                stack.push(rst);
-            } else if (currentChar == '*') {
-                double n1 = stack.pop();
-                double n2 = stack.pop();
-                double rst = n2 * n1;
-                stack.push(rst);
-            } else if (currentChar == '/') {
-                double n1 = stack.pop();
-                double n2 = stack.pop();
-                double rst = n2 / n1;
-                stack.push(rst);
+            if (ch == '(') {
+                if (str.charAt(i+1) == ')') {
+                    cnt += stack_len;
+                    i++;
+                }
+                else {
+                    stack.push(ch);
+                    stack_len++;
+                    cnt++;    
+                }
             } else {
-                if (!map.containsKey(currentChar)) 
-                    map.put(currentChar, arr[cnt++]); 
-                stack.push((double)map.get(currentChar));
+                stack.pop();
+                stack_len--;
             }
         }
 
-        System.out.printf("%.2f",stack.pop());
+        System.out.println(cnt);
 
     }
 }
