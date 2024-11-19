bf3c5eea51e21ca9dbfd9075a06ece60561abce2
Jeo Ahn <ach_toto24@naver.com>
2024-11-19 23:05:01 +0900

    2024.11.19

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.Stack;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Stack<Integer> stack = new Stack<>();
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        String[] arr = new String[str.length()];
+
+        for (int i = 0; i < str.length(); i++) 
+            arr[i] = str.substring(i);
+         
+        Arrays.sort(arr);
+
+        for (String i : arr)
+            System.out.println(i);        
         
-        int[] count = new int[1_000_001];
-        int[] result = new int[N];
-        int[] arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            count[arr[i]]++;
-        }
-
-        for (int i = 0; i < N; i++) {
-            while (!stack.isEmpty() && count[arr[i]] > count[arr[stack.peek()]])
-                result[stack.pop()] = arr[i];
-            stack.push(i);
-        }        
-
-        while (!stack.isEmpty())
-            result[stack.pop()] = -1;
-        
-        StringBuilder sb = new StringBuilder();
-        for (int i : result)
-            sb.append(i).append(" ");
-
-        System.out.println(sb);
-
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,75 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
 
 public class Test {
     
-    // 연산자 우선순위 반환
-    public static int getPriority(char operator) {
-        if (operator == '+' || operator == '-') {
-            return 1;
-        } else if (operator == '*' || operator == '/') {
-            return 2;
-        }
-        return 0;
-    }
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws IOException {
-        Stack<Character> stack = new Stack<>(); // 연산자 스택
-        StringBuilder sb = new StringBuilder(); // 결과 문자열
+        String[] s1 = new String[2];
+        s1[0] = "cda";
+        s1[1] = "cd";
         
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-
-        for (int i = 0; i < str.length(); i++) {
-            char currentChar = str.charAt(i);
-
-            switch (currentChar) {
-                case '+':
-                case '-':
-                    // +, - 연산자 처리
-                    while (!stack.isEmpty() && getPriority(stack.peek()) >= getPriority(currentChar)) {
-                        sb.append(stack.pop());
-                    }
-                    stack.push(currentChar);
-                    break;
-                
-                case '*':
-                case '/':
-                    // *, / 연산자 처리
-                    while (!stack.isEmpty() && getPriority(stack.peek()) >= getPriority(currentChar)) {
-                        sb.append(stack.pop());
-                    }
-                    stack.push(currentChar);
-                    break;
-
-                case '(':
-                    // 여는 괄호는 스택에 넣음
-                    stack.push(currentChar);
-                    break;
-
-                case ')':
-                    // 닫는 괄호가 나오면 여는 괄호까지 스택에서 꺼내어 출력
-                    while (!stack.isEmpty() && stack.peek() != '(') {
-                        sb.append(stack.pop());
-                    }
-                    stack.pop(); // '(' 제거
-                    break;
-
-                default:
-                    // 피연산자일 경우 그대로 결과에 추가
-                    sb.append(currentChar);
-                    break;
-            }
-        }
-
-        // 남아있는 연산자 처리
-        while (!stack.isEmpty()) {
-            sb.append(stack.pop());
-        }
-
-        System.out.println(sb);
+        Arrays.sort(s1);
+        System.out.println(s1[0] + " " + s1[1]);
+        
     }
 }
 
