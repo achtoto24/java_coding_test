17c15439ad115e3a3993c8d7aa805be53783c05d
Jeo Ahn <ach_toto24@naver.com>
2024-11-22 23:05:33 +0900

    2024.11.22

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// GCD 알고리즘
 public class Main {
 
+    public static boolean[] prime;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        prime = new boolean[N + 1];
+        get_prime();
 
-        int d = gcd(a, b);
+        StringBuilder sb = new StringBuilder();
+        for (int i = M; i <= N; i++)
+            if (!prime[i]) sb.append(i).append('\n');
 
-        System.out.println(d);
-        System.out.println(a * b / d);
-               
+        System.out.println(sb);
+
     }
 
-    public static int gcd(int a, int b) {
-
-        if (b == 0)
-            return a;
-
-        return gcd(b, a % b);
+    public static void get_prime() {
         
+        prime[0] = prime[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if (prime[i]) continue;
+            for (int j = i * i; j < prime.length; j += i)
+                prime[j] = true;
+        }
+
     }
 }
