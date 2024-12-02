9d45f3a22b2b03bc27d5d0183f7c68059ed646ec
Jeo Ahn <ach_toto24@naver.com>
2024-12-02 23:31:44 +0900

    2024.12.02

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,27 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
+/*
+ * 59를 예로 들어보자. -> 59! = 59 * 58 * 57 * ... * 1;
+ * 59!의 0의 개수는 2 * 5의 조합의 개수 이므로 2와 5의 소인수의 개수를 구해 구하면 된다.
+ * 소인수 개수에서 2의 개수는 5의 개수보다 항상 많다. 
+ * 따라서 5의 개수를 구하면 된다.
+ * -> 59 / 5 = 11, 11 / 5 = 2 (두번 나눠주는 이유는 5*5나 5*5*2와 같이 5가 두 번 이상 곱해진 수가 있기 때문)
+ */
 public class Main {
-    
-    public static boolean[] prime = new boolean[1000001];
-    
+        
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                
-        prime_func();
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.parseInt(br.readLine());
 
-        while (true) {
-            int N = Integer.parseInt(br.readLine());       
-            if (N == 0) break;
-
-            boolean flag = true;
+            int count = 0;
+            while (N >= 5) {
+                count += N / 5;
+                N /= 5;
+            }    
             
-            for (int i = 2; i < N / 2; i++) {   // -> i는 N - i와 짝을 이루므로 N / 2까지 하면 됨
-                if (!prime[i] && !prime[N - i]) {
-                    System.out.printf("%d = %d + %d\n", N, i, N - i);
-                    flag = false;
-                    break;
-                } 
-            }
-
-            if (flag) System.out.println("Goldbach's conjecture is wrong.");
-            
-        }
-        
+            System.out.println(count);
     }
 
-    public static void prime_func() {
-        prime[0] = prime[1] = true;
-
-		for (int i = 2; i <= (int) Math.sqrt(1000000); i++) { 
-			for (int j = i * i; j < 1000001; j += i) 
-				prime[j] = true;
-		}
-
-    }
 }
