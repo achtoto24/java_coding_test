57758ce263128d05754da7b22c1b2f91ea1f9ffd
Jeo Ahn <ach_toto24@naver.com>
2024-12-09 23:09:25 +0900

    2024.12.09

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,21 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 public class Main {
-        
+    
     public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            int t = Integer.parseInt(br.readLine());
-            
-            for (int i = 0; i < t; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                int n = Integer.parseInt(st.nextToken());
-                int[] arr = new int[n];
-
-                for (int j = 0; j < n; j++)
-                    arr[j] = Integer.parseInt(st.nextToken());
-                    
-                long sum = 0;
-                for (int k = 0; k < n - 1; k++) {
-                    for (int l = k + 1; l < n; l++) 
-                        sum += gcd(arr[k], arr[l]);
-                }
                 
-                System.out.println(sum);
-            }
-                     
+
+
+        
     }
+    
 
-    public static int gcd(int a, int b) {
-
-        if (b == 0) return a;
-
-        return gcd(b, a % b);
-
-    }
 
 }
 
