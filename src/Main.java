324c40cce0599fc964a46441e751e99e00dd7a6c
Jeo Ahn <ach_toto24@naver.com>
2024-12-10 23:30:44 +0900

    2024.12.10

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,18 @@ public class Main {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-                
-
-
+        if (N == 0) sb.append(0);
+        else {
+            while (N != 1) {
+                sb.append(Math.abs(N % -2));
+                N = (int)(Math.ceil((double)N / -2));
+            }
+            sb.append(N);
+        }
+            
+        System.out.println(sb.reverse());
         
     }
     
