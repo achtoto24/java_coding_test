51c626fa350f1f8a1ab18458938e27932c3dd33f
Jeo Ahn <ach_toto24@naver.com>
2024-12-23 22:57:02 +0900

    2024.12.23

--- src/Main.java
+++ src/Main.java
@@ -3,70 +3,58 @@ package java_coding_test.src;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[][] arr;
-    static boolean[][] check;
-    static int[] move_x = {-1, 1, 0, 0};
-    static int[] move_y = {0, 0, -1, 1};
+    static int[] arr = new int[100001];  // 방문 거리 기록
+    static boolean[] check = new boolean[100001];  // 방문 여부
 
-    static int M, N, node;
-    
+    static Queue<Integer> que = new LinkedList<>();
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < T; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            M = Integer.parseInt(st.nextToken());
-            N = Integer.parseInt(st.nextToken());
-            node = Integer.parseInt(st.nextToken()); 
-
-            arr = new int[M + 1][N + 1];
-            check = new boolean[M + 1][N + 1];
-            int cnt = 0;
-
-            for (int j = 0; j < node; j++) {
-                StringTokenizer st2 = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st2.nextToken());
-                int b = Integer.parseInt(st2.nextToken());
-                arr[a][b] = 1;
-            }
-
-            for (int k = 0; k < M; k++) {
-                for (int l = 0; l < N; l++) {
-                    if (arr[k][l] == 1 && !check[k][l]) {
-                        dfsR(k, l);
-                        cnt++;
-                    }
-                }
-            }
-
-            System.out.println(cnt);
-
-        }
-
+        System.out.println(bfs(N, K));
 
     }
 
-    public static void dfsR(int x, int y) {
+    static int bfs(int start, int end) {
+        que.offer(start);
+        check[start] = true;
+        arr[start] = 0; 
 
-        check[x][y] = true;
+        while (!que.isEmpty()) {
+            start = que.poll();
 
-        for (int i = 0; i < 4; i++) {
-            int next_x = x + move_x[i];
-            int next_y = y + move_y[i];
-            
-            if (next_x < 0 || next_y < 0 || next_x > M || next_y > N ) continue;
-            if (arr[next_x][next_y] == 0 || check[next_x][next_y]) continue;
+            if (start == end) {
+                return arr[start];
+            }
 
-            dfsR(next_x, next_y);
+            if (start - 1 >= 0 && !check[start - 1]) {  
+                que.offer(start - 1);
+                check[start - 1] = true;
+                arr[start - 1] = arr[start] + 1;  
+            }
+            if (start + 1 <= 100000 && !check[start + 1]) {  
+                que.offer(start + 1);
+                check[start + 1] = true;
+                arr[start + 1] = arr[start] + 1;  
+            }
+            if (start * 2 <= 100000 && !check[start * 2]) {  
+                que.offer(start * 2);
+                check[start * 2] = true;
+                arr[start * 2] = arr[start] + 1;  
+            }
         }
 
+        return -1;  
     }
-
 }
