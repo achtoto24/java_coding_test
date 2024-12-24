0841568f0a23ebfe456da71ca11c57f91fb784e2
Jeo Ahn <ach_toto24@naver.com>
2024-12-24 23:33:39 +0900

    2024.12.24

--- src/Main.java
+++ src/Main.java
@@ -3,58 +3,57 @@ package java_coding_test.src;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] arr = new int[100001];  // 방문 거리 기록
-    static boolean[] check = new boolean[100001];  // 방문 여부
+    static ArrayList<Integer> list[];
+    static boolean[] check;
 
-    static Queue<Integer> que = new LinkedList<>();
+    static HashMap<Integer, Integer> map = new HashMap<>();
 
+    static int N;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
+        list = new ArrayList[N + 1];
+        check = new boolean[N + 1];
 
-        System.out.println(bfs(N, K));
-
-    }
-
-    static int bfs(int start, int end) {
-        que.offer(start);
-        check[start] = true;
-        arr[start] = 0; 
-
-        while (!que.isEmpty()) {
-            start = que.poll();
-
-            if (start == end) {
-                return arr[start];
-            }
-
-            if (start - 1 >= 0 && !check[start - 1]) {  
-                que.offer(start - 1);
-                check[start - 1] = true;
-                arr[start - 1] = arr[start] + 1;  
-            }
-            if (start + 1 <= 100000 && !check[start + 1]) {  
-                que.offer(start + 1);
-                check[start + 1] = true;
-                arr[start + 1] = arr[start] + 1;  
-            }
-            if (start * 2 <= 100000 && !check[start * 2]) {  
-                que.offer(start * 2);
-                check[start * 2] = true;
-                arr[start * 2] = arr[start] + 1;  
-            }
+        for (int i = 1; i < N+1; i++) {
+            list[i] = new ArrayList<>();
         }
 
-        return -1;  
+        for (int i = 0; i < N-1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            list[a].add(b);
+            list[b].add(a);
+        }
+
+        dfsR(1, 0);
+
+        for (int i = 2; i <= N; i++) {
+            System.out.println(map.get(i));
+        }
+
     }
+
+    static void dfsR(int start, int parent) {
+
+        map.put(start, parent);
+        check[start] = true;
+
+        for (int i : list[start]) {
+            if (!check[i]) dfsR(i, start);
+        }
+
+    }
+
 }
