51a5560b48659e066fcb23b6d723de21bd2e356f
Jeo Ahn <ach_toto24@naver.com>
2024-12-26 22:38:01 +0900

    2024.12.26

--- src/Main.java
+++ src/Main.java
@@ -3,73 +3,65 @@ package java_coding_test.src;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int[][] map;
     static boolean[][] check;
+    static int[] move_x = {-1, 1, 0, 0, -1, -1, 1, 1};
+    static int[] move_y = {0, 0, -1, 1, -1, 1, -1, 1};
 
-    static int[] move_x = {-1, 1, 0, 0};
-    static int[] move_y = {0, 0, -1, 1};
-
-    static int N;
+    static int w, h;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        w = Integer.parseInt(st.nextToken());
+        h = Integer.parseInt(st.nextToken());
 
-        map = new int[N][N];
-
-        int maxDepth = 0;
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] > maxDepth) maxDepth = map[i][j];
+        while (w != 0 && h != 0) {
+            map = new int[h][w];
+            check = new boolean[h][w];
+            int cnt = 0;
+            
+            for (int i = 0; i < h; i++) {
+                StringTokenizer st2 = new StringTokenizer(br.readLine());
+                for (int j = 0; j < w; j++) {
+                    map[i][j] = Integer.parseInt(st2.nextToken());
+                }
             }
-        }
-                
-        int max = 0;
-        for (int depth = 0; depth <= maxDepth; depth++) {
-            check = new boolean[N][N];
-            int area = 0;
 
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (map[i][j] > depth && !check[i][j]) {
-                        dfsR(i, j, depth);
-                        area++;
-                    } 
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (map[i][j] == 1 && !check[i][j]) cnt += dfsR(i, j);
                 }
             }
             
-            max = Math.max(area, max);
+            System.out.println(cnt);
+
+            StringTokenizer st3 = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st3.nextToken());
+            h = Integer.parseInt(st3.nextToken());
         }
-
-        System.out.println(max);
-
     }
 
-    static void dfsR(int x, int y, int depth) {
+    static int dfsR(int x, int y) {
 
         check[x][y] = true;
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 8; i++) {
             int next_x = move_x[i] + x;
             int next_y = move_y[i] + y;
 
-            if (next_x < 0 || next_y < 0 || next_x >= N || next_y >= N) continue;
-            if (map[next_x][next_y] <= depth || check[next_x][next_y]) continue;
+            if (next_x < 0 || next_y < 0 || next_x >= h || next_y >= w) continue;
+            if (map[next_x][next_y] == 0 || check[next_x][next_y]) continue;
 
-            dfsR(next_x, next_y, depth);
+            dfsR(next_x, next_y);
         }
 
+        return 1;
     }
-
 }
-
