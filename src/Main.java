d8426ec38acb1c6c0b59b8777c532f6fd66d4544
Jeo Ahn <ach_toto24@naver.com>
2024-12-28 21:59:16 +0900

    2024.12.28

--- src/Main.java
+++ src/Main.java
@@ -3,86 +3,85 @@ package java_coding_test.src;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[][] map;
+    static int[][] arr;
     static boolean[][] check;
 
-    static int[] move_x = {-2, 2, -1, 1, -2, 2, -1, 1};
-    static int[] move_y = {1, 1, 2, 2, -1, -1, -2, -2};
+    static int[] move_x = {-1, 1, 0, 0};
+    static int[] move_y = {0, 0, -1, 1};
 
-    static int N;
-    static int start_x, start_y, last_x, last_y;
 
-    
+    static int M, N, area;
+
     public static void main(String[] args) throws IOException{
-        
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int T = Integer.parseInt(br.readLine());
-        
-        while (T != 0) {
-            
-            N = Integer.parseInt(br.readLine());
-            
-            StringTokenizer st = new StringTokenizer(br.readLine());    
-            start_x = Integer.parseInt(st.nextToken());
-            start_y = Integer.parseInt(st.nextToken());
-            
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        arr = new int[M][N];
+        check = new boolean[M][N];
+
+        for (int i = 0; i < K; i++) {
             StringTokenizer st2 = new StringTokenizer(br.readLine());
-            last_x = Integer.parseInt(st2.nextToken()); 
-            last_y = Integer.parseInt(st2.nextToken()); 
-            
-            map = new int[N][N];
-            check = new boolean[N][N];
-            
-            System.out.println(bfs(start_x, start_y, last_x, last_y));
-            
-            T--;
-        }
-    }
-    
-    static int bfs(int start_x, int start_y, int last_x, int last_y) {
-        
-        Queue<Spot> que = new LinkedList<>();
-        que.offer(new Spot(start_x, start_y, 0));
-        check[start_x][start_y] = true;
+            int x1 = Integer.parseInt(st2.nextToken());
+            int y1 = Integer.parseInt(st2.nextToken());
+            int x2 = Integer.parseInt(st2.nextToken());
+            int y2 = Integer.parseInt(st2.nextToken());
 
-        while (!que.isEmpty()) {
-            Spot s = que.poll();
-
-            if (s.x == last_x && s.y == last_y) return s.cnt;
-
-            for (int i = 0; i < 8; i++) {
-                int next_x = move_x[i] + s.x;
-                int next_y = move_y[i] + s.y;
-
-                if (next_x < 0 || next_y < 0 || next_x >= N || next_y >= N) continue;
-                if (check[next_x][next_y]) continue;
-
-                que.offer(new Spot(next_x, next_y, s.cnt + 1));
-                check[next_x][next_y] = true;
+            for (int j = x1; j < x2; j++) {
+                for (int k = y1; k < y2; k++) {
+                    arr[k][j] = 1;  
+                }
             }
-
         }
 
-        return - 1;
+        int res = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] == 0 && !check[i][j]) {
+                    area = 0;
+                    dfsR(i, j);
+                    list.add(area);
+                    res++;
+                } 
+                    
+            }
+        }
+
+        Collections.sort(list);
+        
+        System.out.println(res);
+        for (int i = 0 ; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
 
     }
 
+    static void dfsR(int x, int y) {
 
-}
+        check[x][y] = true;
+        area++;
 
-class Spot {
-    int x, y, cnt;
+        for (int i = 0; i < 4; i++) {
+            int next_x = move_x[i] + x;
+            int next_y = move_y[i] + y;
 
-    Spot(int x, int y, int cnt) {
-        this.x = x;
-        this.y = y;
-        this.cnt = cnt;
+            if (next_x < 0 || next_y < 0 || next_x >= M || next_y >= N) continue;
+            if (arr[next_x][next_y] == 1 || check[next_x][next_y]) continue;
+
+            dfsR(next_x, next_y);
+        }
+
     }
+
 }
