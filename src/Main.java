134002046504c2c63649b342e9a1c09cc4583c67
achtoto <ach_toto24@naver.com>
2025-01-02 18:17:00 +0900

    2025.01.02

--- src/Main.java
+++ src/Main.java
@@ -1,87 +1,51 @@
-package java_coding_test.src;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int[][] arr;
-    static boolean[][] check;
+    static boolean[] check;
 
-    static int[] move_x = {-1, 1, 0, 0};
-    static int[] move_y = {0, 0, -1, 1};
-
-
-    static int M, N, area;
-
-    public static void main(String[] args) throws IOException{
+    static int N, line;
+    static int cnt;
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        arr = new int[M][N];
-        check = new boolean[M][N];
-
-        for (int i = 0; i < K; i++) {
-            StringTokenizer st2 = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st2.nextToken());
-            int y1 = Integer.parseInt(st2.nextToken());
-            int x2 = Integer.parseInt(st2.nextToken());
-            int y2 = Integer.parseInt(st2.nextToken());
-
-            for (int j = x1; j < x2; j++) {
-                for (int k = y1; k < y2; k++) {
-                    arr[k][j] = 1;  
-                }
-            }
-        }
-
-        int res = 0;
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if (arr[i][j] == 0 && !check[i][j]) {
-                    area = 0;
-                    dfsR(i, j);
-                    list.add(area);
-                    res++;
-                } 
-                    
-            }
-        }
-
-        Collections.sort(list);
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
         
-        System.out.println(res);
-        for (int i = 0 ; i < list.size(); i++) {
-            System.out.print(list.get(i) + " ");
+        arr = new int[N + 1][N + 1];
+        check = new boolean[N + 1];
+        line = Integer.parseInt(br.readLine());
+        for (int i = 0; i < line; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st2.nextToken());
+            int b = Integer.parseInt(st2.nextToken());
+            
+            arr[a][b] = arr[b][a] = 1;
         }
 
+        cnt = 0;
+        System.out.println(dfsR(num1, num2));
+        
     }
 
-    static void dfsR(int x, int y) {
+    static int dfsR(int start, int end) {
 
-        check[x][y] = true;
-        area++;
-
-        for (int i = 0; i < 4; i++) {
-            int next_x = move_x[i] + x;
-            int next_y = move_y[i] + y;
-
-            if (next_x < 0 || next_y < 0 || next_x >= M || next_y >= N) continue;
-            if (arr[next_x][next_y] == 1 || check[next_x][next_y]) continue;
-
-            dfsR(next_x, next_y);
+        check[start] = true;
+        if (start == end) return cnt;
+        cnt++;
+        
+        for (int i = 1; i <= N; i++) {
+            if (arr[start][i] == 1 && !check[i]) dfsR(i, end);
         }
+        
+        
 
     }
-
 }
--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,3 @@
-package java_coding_test.src;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
