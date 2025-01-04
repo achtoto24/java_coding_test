7ac1dced0ad2aaf4fe34d2f2279569ce30a86e79
Jeo Ahn <ach_toto24@naver.com>
2025-01-05 00:01:42 +0900

    2025.01.05

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,51 @@
+package java_coding_test.src;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[][] arr;
-    static boolean[] check;
+    static long A, B;
+    static HashMap<Long, Integer> check = new HashMap<>();
+    static Queue<Long> que = new LinkedList<>();
 
-    static int N, line;
-    static int cnt;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int num1 = Integer.parseInt(st.nextToken());
-        int num2 = Integer.parseInt(st.nextToken());
-        
-        arr = new int[N + 1][N + 1];
-        check = new boolean[N + 1];
-        line = Integer.parseInt(br.readLine());
-        for (int i = 0; i < line; i++) {
-            StringTokenizer st2 = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st2.nextToken());
-            int b = Integer.parseInt(st2.nextToken());
-            
-            arr[a][b] = arr[b][a] = 1;
-        }
+        A = Long.parseLong(st.nextToken());
+        B = Long.parseLong(st.nextToken());
 
-        cnt = 0;
-        System.out.println(dfsR(num1, num2));
-        
+        System.out.println(bfs(A));
+
     }
 
-    static int dfsR(int start, int end) {
+    static int bfs(long start) {
 
-        check[start] = true;
-        if (start == end) return cnt;
-        cnt++;
-        
-        for (int i = 1; i <= N; i++) {
-            if (arr[start][i] == 1 && !check[i]) dfsR(i, end);
+        que.offer(start);
+        check.put(start, 1);
+
+        while (!que.isEmpty()) {
+            start = que.poll();
+
+            if (start == B) return check.get(start);
+
+            if (start * 2 <= B && !check.containsKey(start * 2)) {
+                que.offer(start * 2);
+                check.put(start * 2, check.get(start) + 1);
+            }
+            if (start * 10 + 1 <= B && !check.containsKey(start * 10 - 1)) {
+                que.offer(start * 10 + 1);
+                check.put(start * 10 + 1, check.get(start) + 1);
+            }
         }
         
-        
-
+        return -1;
     }
 }
