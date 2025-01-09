c77f8622ee121c0e6e8cf903f86b0338cd9f9297
Jeo Ahn <ach_toto24@naver.com>
2025-01-09 23:09:39 +0900

    2025.01.09

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,16 @@ package java_coding_test.src;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static char[][] arr;
-    static boolean[][] check;
+    static boolean[] check = new boolean[26];
+
     static int[][] pos = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    
-    static ArrayList<Character> list = new ArrayList<>();
-    static int R, C, cnt;
+    static int R, C;
+    static int result = 0;
 
     public static void main(String[] args) throws IOException {
 
@@ -27,38 +26,37 @@ public class Main {
         for (int i = 0; i < R; i++) {
             String str = br.readLine();
             for (int j = 0; j < C; j++) {
-                arr[i][j] = str.charAt(j);
-            } 
-        }
-
-        check = new boolean[R][C];
-        cnt = 1;
-        dfsR(0, 0);
-
-        System.out.println(cnt);
-
-    }
-
-    public static void dfsR(int x, int y) {
-
-        check[x][y] = true;
-        char temp = arr[x][y];
-        list.add(temp);
-
-        a :
-        for (int i = 0; i < 4; i++) {
-            int next_x = pos[i][0] + x; 
-            int next_y = pos[i][1] + y; 
-
-            if (next_x < 0 || next_y < 0 || next_x >= R || next_y >= C) continue;
-            for (int j = 0; j < list.size(); j++) {
-                if (arr[next_x][next_y] == list.get(j)) continue a;
+                arr[i][j] = str.charAt(j);                
             }
-            if (check[next_x][next_y]) continue;
+        }
+        
+        dfsR(0, 0, 0);
+        
+        System.out.println(result);
 
-            cnt++;
-            dfsR(next_x, next_y);
+    }
+
+    // 백트래킹 + dfs 알고리즘
+    static void dfsR(int x, int y, int cnt) {
+         
+        if (check[arr[x][y] - 'A']) {
+            result = Math.max(cnt, result);
+            return;
+        } else {
+            check[arr[x][y] - 'A'] = true;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = pos[i][0] + x; 
+                int ny = pos[i][1] + y; 
+
+                if (nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
+
+                dfsR(nx, ny, cnt +1);
+            }
+
+            check[arr[x][y] - 'A'] = false;
         }
 
     }
+
 }
