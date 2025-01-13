5d0115c4bff6c0e8a1a800ee5ae92e65f9909f9e
Jeo Ahn <ach_toto24@naver.com>
2025-01-13 22:55:22 +0900

    2025.01.13

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,19 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[][] arr;
-    static boolean[][] check;
-    static int[][] pos = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    static int[][][] arr;
+    static boolean[][][] check;
+    static int[][] pos = {
+                          {-1, 0, 0}, {1, 0, 0}, {0, -1, 0},
+                          {0, 1, 0}, {0, 0, -1}, {0, 0, 1}
+                         };
 
-    static int M, N, label;
-    static int cnt = 0;
-
-    static Queue<Node> que = new LinkedList<>(); 
-
+    static int M, N, H;
+    static int day = 0;
+    static int label = 1;
+    
+    static Queue<Node> que = new LinkedList<>();
+    
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,80 +29,82 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        
-        arr = new int[N][M];
-        check = new boolean[N][M];
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st2 = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(st2.nextToken());
-            }
-        }
+        H = Integer.parseInt(st.nextToken());
 
-        int cnt2 = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (arr[i][j] == 1)  {
-                    que.offer(new Node(i, j, 1));
-                    cnt2++;
+        arr = new int[H][N][M];
+        check = new boolean[H][N][M];
+        int cnt = 0;
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < N; j++) {
+                StringTokenizer st2 = new StringTokenizer(br.readLine());
+                for (int k = 0; k < M; k++) {
+                    arr[i][j][k] = Integer.parseInt(st2.nextToken());
+                    if(arr[i][j][k] == 1) que.offer(new Node(j, k, i, label));
+                    else if (arr[i][j][k] == 0) cnt++;
                 }
             }
         }
 
-        if (cnt2 == N * M) {
+        if (cnt != 0) {
+            bfs();
+        } else {
             System.out.println(0);
             return;
-        } else bfs();
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (arr[i][j] == 0 && !check[i][j])  {
-                    System.out.println(-1); 
-                    return;
+        }
+        
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < M; k++) {
+                    if (arr[i][j][k] == 0 && !check[i][j][k]) {
+                        System.out.println(-1);
+                        return;
+                    }
                 }
             }
         }
 
-        System.out.println(cnt - 1);
-
+        System.out.println(day);
+        
+                
     }
 
     static void bfs() {
 
-        int temp_label = label;
-        
+        int temp_label = 1;
         while (!que.isEmpty()) {
             Node n = que.poll();
-            if (n.label != temp_label) {
-                cnt++;
+            if (temp_label != n.label) {
                 temp_label = n.label;
-                label++;
-            } 
+                day++;
+            }
             label = n.label + 1;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 6; i++) {
                 int nx = pos[i][0] + n.x;
                 int ny = pos[i][1] + n.y;
+                int nz = pos[i][2] + n.z;
 
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                if (arr[nx][ny] == -1 || check[nx][ny]) continue;
-                    
-                que.offer(new Node(nx, ny, label));
-                check[nx][ny] = true;
+                if (nx < 0 || ny < 0 || nz < 0 || nx >= N || ny >= M || nz >= H) continue;
+                if (arr[nz][nx][ny] == -1 || check[nz][nx][ny]) continue;
+                
+                check[nz][nx][ny] = true;
+                que.offer(new Node(nx, ny, nz, label));
             }
+
         }
         
     }
-   
+
 }
 
 class Node {
+    
+    int x, y, z, label;
 
-    int x, y, label;
-
-    Node (int x, int y, int label) {
+    Node(int x, int y, int z, int label) {
         this.x = x;
         this.y = y;
+        this.z = z;
         this.label = label;
     }
 
