f9229b32a906b14116eec67fa5bbd57799bbeb0f
Jeo Ahn <ach_toto24@naver.com>
2025-01-17 22:47:23 +0900

    2025.01.17

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,18 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-
     static int[][] arr, arr2;
     static boolean[][] check;
     static int[][] pos = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     static int N, M;
-    static int max_value = Integer.MIN_VALUE;
+    static int min_len = Integer.MAX_VALUE;
+    static int no_end_cnt = 0;
     static Queue<Node> que = new LinkedList<>();
+    static Queue<Pos> que2 = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
-        
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -28,55 +29,42 @@ public class Main {
 
         arr = new int[N][M];
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            String str = br.readLine();
             for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = str.charAt(j) - '0';
+                if (arr[i][j] == 1) que2.offer(new Pos(i, j));
             }
         }
+        
+        break_wall();
 
-        dfsR(0); 
-
-        System.out.println(max_value);
-
+        if (no_end_cnt == que2.size()) System.out.println(-1);
+        else System.out.println(min_len);
+        
     }
 
-    static void dfsR(int cnt) {
+    static void break_wall() {
 
-        if (cnt == 3) {
-            arr2 = new int[N][M];
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    arr2[i][j] = arr[i][j];
-                }
-            }
+        for (int i = 0; i < que2.size(); i++) {
             check = new boolean[N][M];
-
+            Pos p = que2.poll();
+            arr[p.x][p.y] = 0;
             bfs();
-            return;
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (arr[i][j] == 0) {
-                    arr[i][j] = 1;
-                    dfsR(cnt + 1);
-                    arr[i][j] = 0;
-                }
-            }
+            arr[p.x][p.y] = 1;
         }
 
     }
 
     static void bfs() {
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (arr[i][j] == 2) que.offer(new Node(i, j));
-            }
-        }
+        int len = 0;
+        que.offer(new Node(0, 0, len));
+        check[0][0] = true;
 
         while (!que.isEmpty()) {
             Node n = que.poll();
+            if (len == n.len) len++;
+            else n.len = len;
 
             for (int i = 0; i < 4; i++) {
                 int nx = pos[i][0] + n.x;
@@ -85,39 +73,47 @@ public class Main {
                 if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
                 if (arr[nx][ny] != 0 || check[nx][ny]) continue;
 
-                que.offer(new Node(nx, ny));
+                que.offer(new Node(nx, ny, len));
                 check[nx][ny] = true;
-                arr2[nx][ny] = 2;
             }
         }
 
-        result_max();
+        if (!check[N-1][M-1]) {
+            no_end_cnt++;
+            return;
+        } else min_length(len);
+
 
     }
 
-    static void result_max() {
+    static void min_length(int temp) {
 
-        int temp_max_value = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (arr2[i][j] == 0) temp_max_value++; 
-            }
-        }
-
-        if (max_value < temp_max_value) max_value = temp_max_value;
+        if (min_len > temp) min_len = temp;
 
     }
-
-
+    
 }
 
-class Node {
+class Pos {
 
     int x, y;
 
-    Node(int x, int y) {
+    Pos (int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+}
+
+
+class Node {
+
+    int x, y, len;
+
+    Node(int x, int y, int len) {
+        this.x = x;
+        this.y = y;
+        this.len = len;
     }
 
 }
