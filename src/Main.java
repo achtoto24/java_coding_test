d068e8868236b748bd056ac3ff739a097fe41eea
Jeo Ahn <ach_toto24@naver.com>
2025-01-28 22:15:51 +0900

    2025.01.28

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,12 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[][] arr, arr2;
-    static boolean[][] check;
+    static int[][] map, dist;
+    static boolean[][][] visit;
     static int[][] pos = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     static int N, M;
-    static int min_len = Integer.MAX_VALUE;
-    static int no_end_cnt = 0;
     static Queue<Node> que = new LinkedList<>();
-    static Queue<Pos> que2 = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
 
@@ -27,93 +24,75 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        arr = new int[N][M];
+        if (N - 1 == 0 && M - 1 == 0) {
+            System.out.println(1);
+            System.exit(0);
+        }
+
+        map = new int[N][M];    // 미로 배열
+        dist = new int[N][M];   // 거리 측정 배열
+        visit = new boolean[2][N][M];   // 벽을 부순 여부에 따라 방문 여부 기록
+
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
             for (int j = 0; j < M; j++) {
-                arr[i][j] = str.charAt(j) - '0';
-                if (arr[i][j] == 1) que2.offer(new Pos(i, j));
+                map[i][j] = str.charAt(j) - '0';
             }
         }
-        
-        break_wall();
 
-        if (no_end_cnt == que2.size()) System.out.println(-1);
-        else System.out.println(min_len);
-        
-    }
-
-    static void break_wall() {
-
-        for (int i = 0; i < que2.size(); i++) {
-            check = new boolean[N][M];
-            Pos p = que2.poll();
-            arr[p.x][p.y] = 0;
-            bfs();
-            arr[p.x][p.y] = 1;
-        }
+        bfs();
 
     }
 
     static void bfs() {
 
-        int len = 0;
-        que.offer(new Node(0, 0, len));
-        check[0][0] = true;
+        que.offer(new Node(0, 0, 0));
 
         while (!que.isEmpty()) {
             Node n = que.poll();
-            if (len == n.len) len++;
-            else n.len = len;
 
             for (int i = 0; i < 4; i++) {
                 int nx = pos[i][0] + n.x;
-                int ny = pos[i][1] + n.y;
+                int ny= pos[i][1] + n.y;
 
                 if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                if (arr[nx][ny] != 0 || check[nx][ny]) continue;
 
-                que.offer(new Node(nx, ny, len));
-                check[nx][ny] = true;
+                if (map[nx][ny] == 1) {                         // 벽이 있을 때 : 벽을 부순적이 있는지, 그 벽을 방문한 적이 있는지 체크
+                    if (n.crash == 0 && !visit[1][nx][ny]) {
+                        visit[n.crash][nx][ny] = true;
+                        dist[nx][ny] = dist[n.x][n.y] + 1;
+                        que.offer(new Node(nx, ny, 1));
+                    }
+                } else {                                        // 벽이 없을 때 : 벽을 부순 여부에 따른 방문을 했는지 체크
+                    if (!visit[n.crash][nx][ny]) {
+                        visit[n.crash][nx][ny] = true;
+                        dist[nx][ny] = dist[n.x][n.y] + 1;
+                        que.offer(new Node(nx, ny, n.crash));
+                    }
+                }
+
+                if (nx == N - 1 && ny == M - 1) {
+                    System.out.println(dist[nx][ny] + 1);
+                    System.exit(0);
+                }
             }
         }
 
-        if (!check[N-1][M-1]) {
-            no_end_cnt++;
-            return;
-        } else min_length(len);
-
-
-    }
-
-    static void min_length(int temp) {
-
-        if (min_len > temp) min_len = temp;
+        System.out.println(-1);
 
     }
     
 }
 
-class Pos {
-
-    int x, y;
-
-    Pos (int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-}
-
 
 class Node {
 
-    int x, y, len;
+    int x, y, crash;
 
-    Node(int x, int y, int len) {
+    Node(int x, int y, int crash) {
         this.x = x;
         this.y = y;
-        this.len = len;
+        this.crash = crash;
     }
 
 }
