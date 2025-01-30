71c78c9009f23ea833086f9544420883a194140a
Jeo Ahn <ach_toto24@naver.com>
2025-01-30 22:52:41 +0900

    2025.01.30

--- src/Main.java
+++ src/Main.java
@@ -10,23 +10,22 @@ public class Main {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < T; i++) {
-            int n = Integer.parseInt(br.readLine());
-            dp = new int[11];
-
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 4;
-
-            for (int j = 4; j <= n; j++) {
-                dp[j] = dp[j - 3] + dp[j - 2] + dp[j - 1];
-            }
-
-            System.out.println(dp[n]);
-
+        int n = Integer.parseInt(br.readLine());
+        
+        if (n == 1) {
+            System.out.println(1);
+            System.exit(0);
         }
+
+        dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
 
     }
 
