61ff59e95e31847d5f8bc4d3bcf2a5fd11564c1c
Jeo Ahn <ach_toto24@naver.com>
2025-02-05 22:48:24 +0900

    2025.02.05

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] dp;
-
     public static void main(String[] args) throws IOException {
-        
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        
-        if (n == 1) {
-            System.out.println(1);
-            System.exit(0);
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
+        // 이중 for문을 통해 기준 값과 기준 이전 값들을 비교하여 값을 갱신함
+        // dp default : 1
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && dp[i] < dp[j] + 1) dp[i] = dp[j] + 1;    
+                // dp[j] + 1을 하는 이유는 이전 부분수열에 j-1번째 원소가 추가되었기 때문
+                // dp[i] > dp[j] + 1이라면 굳이 값을 갱신해줄 필요가 없음
+            }               
+        }
         
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] > max) max = dp[i];
         }
 
-        System.out.println(dp[n]);
-
+        System.out.println(max);
+        
     }
-
 }
