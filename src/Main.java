73e913a14b29bb16e11f6aaccd63e384d5759cf2
Jeo Ahn <ach_toto24@naver.com>
2025-02-08 22:16:24 +0900

    2025.02.08

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 조합 공식(파스칼 공식)을 이용하기
 public class Main {
 
      public static void main(String[] args) throws IOException {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n + 1][n + 1];
-        int[][] dp = new int[n + 1][n + 1];
+        int[][] dp = new int[30][30];
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < 30; i++) {
+            dp[i][i] = 1;
+            dp[i][0] = 1;
+        }
+        
+        for (int i = 2; i < 30; i++) {
+            for (int j = 1; j < 30; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= i; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+            int west = Integer.parseInt(st.nextToken());
+            int east = Integer.parseInt(st.nextToken());
+
+            System.out.println(dp[east][west]);
+            
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j];
-            }
-        }
-
-        int max = Integer.MIN_VALUE;
-        for (int  i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (max < dp[i][j]) max = dp[i][j];
-            }
-        }
-
-        System.out.println(max);
 
      }
 }
