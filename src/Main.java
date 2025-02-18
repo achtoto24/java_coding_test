db8fbe0054d84c56d3be348cd30048f5e71c850d
Jeo Ahn <ach_toto24@naver.com>
2025-02-18 23:05:34 +0900

    2025.02.18

--- src/Main.java
+++ src/Main.java
@@ -8,32 +8,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         
-        int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n + 1][n + 1];
-        int[][] dp = new int[n + 1][n + 1];
-
+        int  n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        int[] dp = new int[n + 1];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= i; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        dp[1] = arr[1];
+        int max = arr[1];
+        if (n >= 2) {
+            for (int i = 2; i <= n; i++) {
+                dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+                max = Math.max(max, dp[i]);
             }
         }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + arr[i][j];
-            }
-        }
-
-        int max = Integer.MIN_VALUE;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (max < dp[i][j]) max = dp[i][j];
-            }
-        }
-
+        
+        // 마지막 dp[n] 값이 max가 되는 게 아니라 dp[1]~dp[n]까지 값들 중 max를 구하는 것
         System.out.println(max);
 
     }
