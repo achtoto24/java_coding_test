d3b96707654eedfc517e2916cfe0f2fae7937b62
Jeo Ahn <ach_toto24@naver.com>
2025-02-22 22:35:37 +0900

    2025.02.22

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         
-        int  n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n + 1];
-        int[] arr = new int[n + 1];
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N + 1][2];
+        int[][] dp = new int[N + 1][2];
 
-        for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        dp[1] = arr[1];
         
-        if (n >= 2) {
-            dp[2] = arr[1] + arr[2];
-            for (int i = 3; i <= n; i++) {
-                dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2], dp[i - 3] + arr[i - 1]) + arr[i]);
-            }
-        }
-
-        System.out.println(dp[n]);
-
+        
     }
 }
