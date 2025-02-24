64016824cbd6f53aa6d41a2a36bd4582398b6a1b
Jeo Ahn <ach_toto24@naver.com>
2025-02-24 22:01:18 +0900

    2025.02.24

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,8 @@ public class Main {
         StringTokenizer st;
         
         int N = Integer.parseInt(br.readLine());
-        int[][] arr = new int[N + 1][2];
-        int[][] dp = new int[N + 1][2];
+        int[][] arr = new int[N + 2][2];
+        int[] dp = new int[N + 2];
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -20,7 +20,17 @@ public class Main {
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i <= N + 1; i++) {
+            if (max < dp[i]) max = dp[i];
+
+            int after = i + arr[i][0];
+            if (after <= N + 1) {
+                dp[after] = Math.max(dp[after], arr[i][1] + max);
+            }
+        }
+
+        System.out.println(max);
         
     }
 }
