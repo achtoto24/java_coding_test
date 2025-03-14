0a621e32871bbe78c229fb168b0b643198da6e99
Jeo Ahn <ach_toto24@naver.com>
2025-03-15 00:37:38 +0900

    2025.03.15:

--- src/Main.java
+++ src/Main.java
@@ -5,49 +5,73 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    
-    static int[] arr;
 
-    public static void main(String[] args) throws IOException {
+    static int[] arr;
+    public static void main(String[] args) throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());        
+        int M = Integer.parseInt(st.nextToken());        
         arr = new int[N];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-
         Arrays.sort(arr);
 
-        int M = Integer.parseInt(br.readLine());
+        int result = binary_Search(M);
+        
+        System.out.println(result);
+    }
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            if (binarySearch(Integer.parseInt(st.nextToken())) != -1) System.out.println(1);
-            else System.out.println(0);
+    static int binary_Search(int key) {
+
+        int li = 0;
+        int ri = arr.length;
+        int len_total = 0;
+        int result = 0;
+
+        while (li < ri) {
+            System.out.printf("li : %d, ri : %d\n", li, ri);
+            int mid = (li + ri) / 2;
+            // System.out.println("mid : " + mid);
+            
+            len_total = 0;
+            for (int i = mid; i < arr.length; i++) {
+                len_total += arr[i] - arr[mid];
+            }
+            System.out.println(len_total);
+
+            if (len_total <= key) ri = mid;
+            else li = mid + 1;
         }
         
-    }
+        if (len_total == key) {
+            result = arr[li];
+            return result;
+        } else {
+            for (int i = arr[li - 1] + 1; i < arr[ri]; i++) {
+                len_total = 0;
+                for (int j = li; j < arr.length; j++) {
+                    len_total += arr[j] - i;
+                }
+                // System.out.println(len_total);
+                if (len_total == key) {
+                    result = i;
+                    break;
+                }
+            }
 
-    public static int binarySearch(int key) {
-        int li = 0;
-        int ri = arr.length - 1;
-
-        while (li <= ri) {
-            int mid = (li + ri) / 2;
-
-            if (key < arr[mid]) ri = mid - 1;
-            else if (key > arr[mid]) li = mid + 1;
-            else return mid;
+            return result;
         }
-
-        return -1;
-
     }
-    
 
+
+    
+    
 }
