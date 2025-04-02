4c0c44a87696f1d6c955f6814f62e0776aa8b787
Jeo Ahn <ach_toto24@naver.com>
2025-04-02 23:00:40 +0900

    2025.04.02

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
         StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
-        long X = Long.parseLong(st.nextToken());
-        long Y = Long.parseLong(st.nextToken());
-       
-        long Z = Y * 100 / X ;
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            int[] arr = new int[N];
 
-        if (Z >= 99) {
-            System.out.println(-1);
-            return;
-        }
-        int min = 0;
-        int max = 1000000000;
-        int result = 0;
-
-        while (min <= max) {
-            int mid = (min + max) / 2;
-
-            long temp = (Y + mid) * 100 / (X + mid);
-
-            if (Z >= temp) {
-                min = mid + 1;
-                result = mid + 1;
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i] = Integer.parseInt(st.nextToken());
             }
-            else max = mid - 1;
+
+            Arrays.sort(arr);
+
+            int M = Integer.parseInt(br.readLine());
+            int[] arr2 = new int[M];
+
+            st = new StringTokenizer(br.readLine());
+
+            a : 
+            for (int j = 0; j < M; j++) {
+                arr2[j] = Integer.parseInt(st.nextToken());
+
+                int left = 0;
+                int right = N - 1;
+                
+                while (left <= right) {
+                    int mid = (left + right) / 2;
+
+                    if (arr2[j] > arr[mid]) left = mid + 1;
+                    else if (arr2[j] < arr[mid]) right = mid - 1;
+                    else {
+                        System.out.println(1);
+                        continue a;
+                    }
+                }
+                
+                System.out.println(0);
+                
+            }
 
         }
-
-        System.out.println(result);
         
     }
 }
