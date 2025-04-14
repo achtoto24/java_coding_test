23952e3f57b43a1a0b32b29f21753a5d35db5539
Jeo Ahn <ach_toto24@naver.com>
2025-04-14 22:21:28 +0900

    2025.04.14

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		long N = Long.parseLong(br.readLine());
-
-		long left = 0;
-		long right = N;
-		long result = 0;
-
-		while (left <= right) {
-			long mid = (left + right) / 2;
-
-			if (Math.pow(mid, 2) >= N) {
-				result = mid;
-				right = mid - 1; 
-			} else left = mid + 1;
-		}
-
-		System.out.println(result);
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[M];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int bottom = 1;
+		int top = N;
+		int result = 0;
+
+		while (bottom <= top) {
+			int mid = (bottom + top) / 2;
+			boolean flag = true;
+
+			int point = 0;
+			for (int i = 0; i < M; i++) {
+				if (arr[i] - mid <= point) point = arr[i] + mid;
+				else flag = false;
+			}
+
+			if (N - point > 0) flag = false;
+			else flag = true;
+
+			if (flag) {					// 모두 비출 수 있음
+				result = mid;
+				top = mid - 1;
+			} else bottom = mid + 1;	// 모두 비출 수 없음
+		}
+		
+		System.out.println(result);
+
 	}
+	
 }
