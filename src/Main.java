a062ab3099994292a7c73a89f3f9ab72a4bbbb9f
Jeo Ahn <ach_toto24@naver.com>
2025-04-21 22:40:34 +0900

    2025.04.21

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[] arr = new int[M];
+		while (true) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < M; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
+			if (N == 0 && M == 0) break;
+			
+			int[] arr1 = new int[N];
+			int[] arr2 = new int[M];
+			
 
-		Arrays.sort(arr);
-
-		int left = 1;
-		int right = arr[M - 1];
-		int result = Integer.MAX_VALUE;
-		while (left <= right) {
-			int mid = (left + right) / 2;
-			int cnt = 0;
+			for (int i = 0; i < N; i++) {
+				arr1[i] =  Integer.parseInt(br.readLine());
+			}
 
 			for (int i = 0; i < M; i++) {
-				if (mid <= arr[i] && arr[i] % mid != 0) cnt += arr[i] / mid + 1; 
-				else if (mid <= arr[i] && arr[i]% mid == 0) cnt += arr[i] / mid;
-				else cnt++;
+				arr2[i] = Integer.parseInt(br.readLine());
 			}
 
-			if (cnt > N) {
-				left = mid + 1;
-			}	
-			else {
-				result = mid;
-				right = mid - 1;
+			int cnt = 0;
+
+			for (int i = 0; i < N; i++) {
+				int num = arr1[i];
+
+				int left = 0;
+				int right = M - 1;
+				while (left <= right) {
+					int mid = (left + right) / 2;
+
+					if (arr2[mid] < num) left = mid + 1;
+					else if (arr2[mid] > num) right = mid - 1;
+					else {
+						cnt++;
+						break;
+					}
+				}
 			}
+
+			System.out.println(cnt);
+
 		}
 		
-		System.out.println(result);
-
 	}
-	
 }
