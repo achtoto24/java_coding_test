5f816f392fcc42f3fea6f4442b911281459ba585
Jeo Ahn <ach_toto24@naver.com>
2025-04-22 22:25:26 +0900

    2025.04.22

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,47 +11,35 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		while (true) {
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
 
-			if (N == 0 && M == 0) break;
-			
-			int[] arr1 = new int[N];
-			int[] arr2 = new int[M];
-			
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
 
-			for (int i = 0; i < N; i++) {
-				arr1[i] =  Integer.parseInt(br.readLine());
-			}
+		Arrays.sort(arr);
 
-			for (int i = 0; i < M; i++) {
-				arr2[i] = Integer.parseInt(br.readLine());
-			}
+		long bottom = 1;
+		long top = arr[N - 1];
 
+		while (bottom <= top) {
+			long mid = (bottom + top) / 2;
 			int cnt = 0;
 
 			for (int i = 0; i < N; i++) {
-				int num = arr1[i];
-
-				int left = 0;
-				int right = M - 1;
-				while (left <= right) {
-					int mid = (left + right) / 2;
-
-					if (arr2[mid] < num) left = mid + 1;
-					else if (arr2[mid] > num) right = mid - 1;
-					else {
-						cnt++;
-						break;
-					}
-				}
+				cnt += arr[i] / mid;
 			}
 
-			System.out.println(cnt);
-
+			if (cnt >= K) {
+				bottom = mid + 1;
+			} else top = mid - 1;
 		}
-		
+
+		System.out.println(top);
+
 	}
+
 }
