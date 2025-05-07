fc61252dae09aaf368d115182e7b128e83489390
Jeo Ahn <ach_toto24@naver.com>
2025-05-07 22:35:32 +0900

    2025.05.07

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,29 @@ import java.util.StringTokenizer;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), "-");
+		int sum = 0;
+		boolean flag = true;
 
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int[] arr = new int[N];
-
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}		
-
-		int cnt = 0;
-		for (int i = N -1; i >= 0; i--) {
-			if (arr[i] <= K) {
-				cnt += K / arr[i];
-				K %= arr[i];
+		while (st.hasMoreTokens()) {
+			StringTokenizer st2 = new StringTokenizer(st.nextToken(), "+");
+			int temp = 0;
+			
+			while (st2.hasMoreTokens()) {
+				temp += Integer.parseInt(st2.nextToken());
 			}
+
+			if (flag) {
+				sum += temp;
+				flag = false;
+			}
+			else sum -= temp;
 		}
 
-		System.out.println(cnt);
+		System.out.println(sum);
 		
 	}
 }
