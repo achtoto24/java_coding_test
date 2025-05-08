196329812a9d71fd9775c472efe9882fdb171e2a
Jeo Ahn <ach_toto24@naver.com>
2025-05-08 22:25:21 +0900

    2025.05.08

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,36 @@ import java.util.StringTokenizer;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
-		StringTokenizer st = new StringTokenizer(br.readLine(), "-");
-		int sum = 0;
-		boolean flag = true;
-
-		while (st.hasMoreTokens()) {
-			StringTokenizer st2 = new StringTokenizer(st.nextToken(), "+");
-			int temp = 0;
-			
-			while (st2.hasMoreTokens()) {
-				temp += Integer.parseInt(st2.nextToken());
-			}
-
-			if (flag) {
-				sum += temp;
-				flag = false;
-			}
-			else sum -= temp;
+		int N = Integer.parseInt(br.readLine());
+		long[] road = new long[N-1];
+		long[] price = new long[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i <N-1; i++) {
+			road[i] = Long.parseLong(st.nextToken());
 		}
 
-		System.out.println(sum);
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			price[i] = Long.parseLong(st.nextToken());
+		}
+
+		long total = 0;
+		long min = price[0];
+		total += price[0] * road[0];
+		for (int i = 1; i < N-1; i++) {
+			if (min > price[i]) {
+				total += price[i] * road[i];
+				min = price[i];
+			} else total += min * road[i];
+		}
+
+		System.out.println(total);
+
 		
 	}
 }
