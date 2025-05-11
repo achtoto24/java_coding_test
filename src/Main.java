5cf7c692c86f10256a7ac97ae63db9047b88dba6
Jeo Ahn <ach_toto24@naver.com>
2025-05-11 23:30:54 +0900

    2025.05.11

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 서류 성적 순위로 정렬한 후 면접 성적 비교하기
+// 비교군 중에서 한 명이라도 서류, 면접 성적 순위가 모두 높다면 탈락! 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st, st2;
 		
-		String S = br.readLine();
-		int num_0 = 0, num_1 = 0;
-		String temp;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-		st = new StringTokenizer(S,"0");
-		 while (st.hasMoreTokens()) {
-			temp = st.nextToken();
-			num_0++;
-		 }
+		int T = Integer.parseInt(br.readLine());
 
-		 
-		st2 = new StringTokenizer(S,"1");
-		while (st2.hasMoreTokens()) {
-			temp = st2.nextToken();
-		   	num_1++;
+		for (int i = 0; i < T; i++) {
+			int N = Integer.parseInt(br.readLine());
+			int[] arr = new int[N + 1];
+			int cnt = 1;
+
+			for (int j = 0; j < N; j++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				arr[a] = b;
+			}
+
+			int compare = arr[1];
+			for (int j = 2; j < N+1; j++) {
+				if (compare > arr[j]) {
+						cnt++;
+						compare = arr[j];
+				}
+			}
+			
+			System.out.println(cnt);
+			
 		}
-
-		if (num_0 >= num_1) System.out.println(num_1);
-		else System.out.println(num_0);;
+		
 		
 	}
 }
