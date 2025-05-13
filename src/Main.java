f44fb1d9924b0d5dc3cb48b132703ab6daed06a9
Jeo Ahn <ach_toto24@naver.com>
2025-05-13 22:44:43 +0900

    2025.05.13

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-		String str = br.readLine();
-		int[] arr = new int[str.length()];
-		for (int i = 0; i < str.length(); i++) {
-			arr[i] = str.charAt(i) - '0';
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int[] arr1 = new int[M];
+		int[] arr2 = new int[M];
+
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr1[i] = Integer.parseInt(st.nextToken());
+			arr2[i] = Integer.parseInt(st.nextToken());
 		}
 
-		Arrays.sort(arr);
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
 
-		int sum = 0;
-		for (int i : arr) {
-			sum += i;
-		}
+		int num1 = N / 6;
+		int num2 = N % 6;
 
-		if (sum % 3 == 0 && arr[0] == 0) {
-			for (int i = arr.length - 1; i >= 0; i--) {
-				sb.append(arr[i]);
-			}
-			System.out.println(sb);
-		} else System.out.println(-1);
-					
+		int cost1 = (num1 + 1) * arr1[0];
+		int cost2 = num1 * arr1[0] + num2 * arr2[0];
+		int cost3 = N * arr2[0];
+		
+		System.out.println(Math.min(cost1, Math.min(cost2, cost3)));
+		
 	}
-	
 }
