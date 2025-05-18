3c4fe65b4de3143099796b301dac7eea36b15167
Jeo Ahn <ach_toto24@naver.com>
2025-05-18 22:50:33 +0900

    2025.05.18

--- src/Main.java
+++ src/Main.java
@@ -9,33 +9,54 @@ public class Main {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
-		int T = Integer.parseInt(br.readLine());
+		int[][] A = new int[N][M];
+		int[][] B = new int[N][M];
 
-		while (T-- > 0) {
-
-			int N = Integer.parseInt(br.readLine());
-			int[] arr = new int[N];
-
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < N; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < M; j++) {
+				A[i][j] = str.charAt(j) - '0';
 			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < M; j++) {
+				B[i][j] = str.charAt(j) - '0';
+			}
+		}
 
-			long profit = 0;
-			int max = 0;
-			for (int i = N - 1; i >= 0; i--) {
-				if (arr[i] > max) {
-					max = arr[i];
-				} else {
-					profit += max - arr[i];
+		int cnt = 0;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (A[i][j] != B[i][j] && i + 2 < N && j + 2 < M) {
+					for (int r = i; r < i + 3; r++) {
+						for (int c = j; c < j + 3; c++) {
+							A[r][c] = A[r][c] == 0 ? 1 : 0;
+						}
+					}
+					cnt++;
 				}
 			}
-
-			System.out.println(profit);
-
 		}
-			
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (A[i][j] != B[i][j]) {
+					System.out.println(-1);
+					return;
+				}
+			}
+		}
+
+		System.out.println(cnt);
+		
 	}
 
-}
+ }
