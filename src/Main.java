209ef0adbf4b02113e230401489607012eaca550
Jeo Ahn <ach_toto24@naver.com>
2025-05-19 22:37:04 +0900

    2025.05.19

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
 
-		int[][] A = new int[N][M];
-		int[][] B = new int[N][M];
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
 
 		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < M; j++) {
-				A[i][j] = str.charAt(j) - '0';
-			}
-		}
-		
-		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < M; j++) {
-				B[i][j] = str.charAt(j) - '0';
-			}
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 
+		int num = arr[N - 1];
 		int cnt = 0;
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (A[i][j] != B[i][j] && i + 2 < N && j + 2 < M) {
-					for (int r = i; r < i + 3; r++) {
-						for (int c = j; c < j + 3; c++) {
-							A[r][c] = A[r][c] == 0 ? 1 : 0;
-						}
-					}
-					cnt++;
-				}
+		for (int i = N-2; i >= 0; i--) {
+			while (arr[i] >= num) {
+				arr[i]--;
+				cnt++;
 			}
+			num = arr[i];
 		}
 		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (A[i][j] != B[i][j]) {
-					System.out.println(-1);
-					return;
-				}
-			}
-		}
-
 		System.out.println(cnt);
 		
 	}
-
- }
+}
