ae2b3131074a82c0bfb8c5db58bcd6619dd8dcac
Jeo Ahn <ach_toto24@naver.com>
2025-05-20 22:44:40 +0900

    2025.05.20

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,51 @@ import java.io.InputStreamReader;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+		int[] arr = new int[26];
+		
+		String str = br.readLine();
+		for (int i = 0; i < str.length(); i++) {
+			int idx = str.charAt(i) - 'A';
+			arr[idx]++;
 		}
 
-		int num = arr[N - 1];
-		int cnt = 0;
-		for (int i = N-2; i >= 0; i--) {
-			while (arr[i] >= num) {
-				arr[i]--;
-				cnt++;
-			}
-			num = arr[i];
+		int isOdd = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] % 2 != 0) isOdd++;
 		}
 		
-		System.out.println(cnt);
+		String result = "";
+		if (isOdd > 1) {
+			result += "I'm Sorry Hansoo";
+		} else {
+			StringBuilder sb = new StringBuilder();
+
+			// start
+			for (int i = 0; i < arr.length; i++) {
+				for (int j = 0; j < arr[i]/2; j++) {
+					sb.append((char)(i + 65));
+				}
+			}
+			result += sb.toString();
+
+			// end 
+			String temp = sb.reverse().toString();
+
+			// middle
+			sb = new StringBuilder();
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] % 2 != 0) sb.append((char)(i + 65));
+			}
+
+			result += sb.toString() + temp;
+			
+		}
+		
+		System.out.println(result);
+
 		
 	}
 }
