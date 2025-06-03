59b72202294fca1b0e2c70cfe24559703f05d175
Jeo Ahn <ach_toto24@naver.com>
2025-06-03 23:10:39 +0900

    2025.06.03

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int num = Integer.parseInt(br.readLine());
 
-        if (num <100) {
-            System.out.println(num);
-        } else {
-            int temp = 100;
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
             int cnt = 0;
-            while (temp <= num) {
-                int a = temp / 100;
-                int b = (temp % 100) / 10;
-                int c = (temp % 100) % 10;
-                
-                if ((b - a) == (c  - b)) 
-                    cnt++;
-
-                temp++;
+            for (int  j = 0; j < N; j++) {
+                if (arr[i][0] > arr[j][0] || arr[i][1] > arr[j][1]) cnt++; 
             }
-
-            System.out.println(99 + cnt);
+            sb.append(N - cnt).append(" ");
         }
         
+        System.out.println(sb);
     }
-
 }
