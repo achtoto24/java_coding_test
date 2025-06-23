635b122512e3648fe59058337c1977945793db6c
Jeo Ahn <ach_toto24@naver.com>
2025-06-23 22:02:45 +0900

    2025.06.23:

--- src/Main.java
+++ src/Main.java
@@ -2,70 +2,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static boolean[] check;
-    static Queue<Spot> que = new LinkedList<>();
-
+    static int N, M;
+    
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int F = Integer.parseInt(st.nextToken());
-        int S = Integer.parseInt(st.nextToken());
-        int G = Integer.parseInt(st.nextToken());
-        int U = Integer.parseInt(st.nextToken());
-        int D = Integer.parseInt(st.nextToken());
 
-        check = new boolean[F + 1];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int result = bfs(F, S, G, U, D);
-        if (result == -1) System.out.println("use the stairs");
-        else System.out.println(result);
+
         
     }
 
-    static int bfs(int height, int start, int end, int up, int down) {
+    static void func() {
 
-        que.offer(new Spot(start, 0));
-        check[start] = true;
-        
-        while (!que.isEmpty()) {
-            Spot spot = que.poll();
+        for (int i = 1; i <= N; i++) {
 
-            if (spot.layer == end) {
-                return spot.cnt;
-            }
-
-            if (spot.layer + up <= height && !check[spot.layer + up]) {
-                que.offer(new Spot(spot.layer + up, spot.cnt + 1));
-                check[spot.layer + up] = true;
-            }
+            func();
             
-            if (spot.layer - down >= 1 && !check[spot.layer - down]) {
-                que.offer(new Spot(spot.layer - down, spot.cnt + 1));
-                check[spot.layer - down] = true;
-            }
         }
-
-        return -1;
-        
     }
     
-}
-
-class Spot {
-
-    int layer, cnt;
-
-    Spot(int layer, int cnt) {
-        this.layer = layer;
-        this.cnt = cnt;
-    }
     
 }
