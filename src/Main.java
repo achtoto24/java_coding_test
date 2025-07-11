e483c90345b0fd3ecde5a89568ab2ca67027ad7c
Jeo Ahn <ach_toto24@naver.com>
2025-07-11 22:08:44 +0900

    2025.07.11

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,12 @@ public class Main {
 
     static int[] arr;
     static boolean[] check;
-
+    
     static int N, M;
-
     static StringBuilder sb = new StringBuilder();
+    
 
-    public static void main(String[] args) throws  IOException {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -27,21 +27,20 @@ public class Main {
         dfs(0);
 
         System.out.println(sb);
-        
     }
 
     static void dfs(int depth) {
 
         if (depth == M) {
             StringBuilder sb2 = new StringBuilder();
-            for (int i = 0; i < M - 1; i++) {
-                if (arr[i] >= arr[i + 1]) return;
+            for (int i = 0; i < M-1; i++) {
+                if (arr[i] > arr[i+1]) return;
                 sb2.append(arr[i] + " ");
             }
-            sb2.append(arr[M - 1]);
+            sb2.append(arr[M-1]);
             sb.append(sb2 + "\n");
             return;
-        }   
+        }
 
         for (int i = 0; i < N; i++) {
             check[i] = true;
@@ -49,8 +48,7 @@ public class Main {
             dfs(depth + 1);
             check[i] = false;
         }
-        
-        
+
     }
 
 }
