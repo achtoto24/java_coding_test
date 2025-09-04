90ef7d9cd2acdb62b9f89ca7c5543b4354cb2b94
achtoto <ach_toto24@naver.com>
2025-09-04 18:37:40 +0900

    2025.09.04

--- src/Main.java
+++ src/Main.java
@@ -5,53 +5,56 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
+    
     static int[] arr, arr2;
     static boolean[] check;
     static int N, M;
     
+    static StringBuilder sb = new StringBuilder();
+    
     public static void main(String[] args) throws IOException {
-
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
+        
         arr = new int[N];
         check = new boolean[N];
         arr2 = new int[M];
-
-        st = new StringTokenizer(br.readLine());
+        
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st2.nextToken());
         }
-                
+        
         Arrays.sort(arr);
-
+        
         dfs(0);
-                
+        
+        System.out.println(sb);
+        
     }
-
+    
     static void dfs(int depth) {
-
+        
+        
         if (depth == M) {
             for (int i = 0; i < M; i++) {
-                System.out.print(arr2[i] + " ");
+                sb.append(arr2[i] + " ");
             }
-            System.out.println();
-        }
-
-        else {
-            int before = 0;
+            sb.append("\n");
+        } else {
             for (int i = 0; i < N; i++) {
-                if (!check[i] && before != arr[i]) {
+                int before = 0; 
+                if (!check[i] && arr[i] != before) {
                     check[i] = true;
-                    arr2[depth] = arr[i];
                     before = arr[i];
+                    arr2[depth] = arr[i];
                     dfs(depth + 1);
                     check[i] = false;
-                 }
+                }
             }
         }
         
