67991b599457a3791655e3d7db7e29b587217185
achtoto <ach_toto24@naver.com>
2025-09-10 15:48:46 +0900

    2025.09.10

--- src/Main.java
+++ src/Main.java
@@ -2,57 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
-    
-    static int[] arr, arr2;
-    static int N, M;
-    
-    static StringBuilder sb = new StringBuilder();
-    
+
     public static void main(String[] args) throws IOException {
-        
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        
-        arr = new int[N];
-        arr2 = new int[M];
-        
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st2.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        int a = Integer.parseInt(br.readLine());
+        if (N == 1) {
+            System.out.println(0);
+            return;
         }
-        
-        Arrays.sort(arr);
-        
-        dfs(0, 0);
-        
-        System.out.println(sb);
+
+        int[] arr = new int[N-1];
+        for (int i = 0; i < N-1; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int cnt = 0;
+
+        while (true) {
+            Arrays.sort(arr);
+
+            if (arr[N-2] < a) break;
+            
+            cnt++;
+            arr[N-2]--;
+            a++;
+        }
+
+        System.out.println(cnt);
         
     }
-    
-    static void dfs(int depth, int before) {
-        
-        
-        if (depth == M) {
-            for (int i = 0; i < M; i++) {
-                sb.append(arr2[i] + " ");
-            }
-            sb.append("\n");
-        } else {
-            for (int i = 0; i < N; i++) {
-                if (arr[i] >= before) {
-                    before = arr[i];
-                    arr2[depth] = arr[i];
-                    dfs(depth + 1, before);
-                }
-            }
-        }
-        
-    }
-    
 }
--- src/Test.java
+++ src/Test.java
@@ -1,55 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Test {
 
-    static int[] arr;
-    static boolean[] check;
-    
-    static int N, M;
-    static StringBuilder sb = new StringBuilder();
-    
+	public static void main(String[] args) {
 
-    public static void main(String[] args) throws IOException {
+		int n = (int)(11 * Math.random());
+		System.out.println("random number : " + n);
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		if (n <= 5) {
+			System.out.println("n : " + n);
+			System.out.println("n*n : " + n*n);
+		}
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        arr = new int[M];
-        check = new boolean[N];
-
-        dfs(0);
-
-        System.out.println(sb);
-    }
-
-    static void dfs(int depth) {
-
-        if (depth == M) {
-            StringBuilder sb2 = new StringBuilder();
-            for (int i = 0; i < M-1; i++) {
-                if (arr[i] > arr[i+1]) return;
-                sb2.append(arr[i] + " ");
-            }
-            sb2.append(arr[M-1]);
-            sb.append(sb2 + "\n");
-            return;
-        }
-
-        for (int i = 0; i < N; i++) {
-            if (!check[i]) {
-                check[i] = true;
-                arr[depth] = i + 1;
-                dfs(depth + 1);
-                check[i] = false;
-            }
-        }
-
-    }
-
+		System.out.println();
+		System.out.println("end");
+		
+	}
 }
