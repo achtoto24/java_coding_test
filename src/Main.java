317736855766b42e96f2bb06ab8ef9c97f50720a
achtoto <ach_toto24@naver.com>
2025-09-19 16:13:34 +0900

    2025.09.19

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
+    static char[] arr;
+    static int cnt = 0;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        int L = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
-
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st2.nextToken());
-        }
-
-        Arrays.sort(arr);
-
-        int i = 0;
-        while (i < N && L >= arr[i]) {    
-            L++;
-            i++;
-        }
+        arr = new char[N];
         
-        System.out.println(L);
+        arr = br.readLine().toCharArray();
+
+        for (int i = 0; i < N; i++) {
+            if (arr[i] == 'P') {
+                int idx = Math.max(i - K, 0);
+                boolean check = false;
+
+                // 'P' 왼쪽 탐색
+                for (int j = idx; j < i; j++) {
+                    if (burgerCheck(j)) {
+                        check = true;
+                        // System.out.println(j);
+                        break;
+                    }
+                }
+                
+                // 'P' 오른쪽 탐색
+                if (!check) {
+                    idx = i+K >= N ? N-1 : i+K;
+                    for (int j = i + 1; j <= idx; j++) {
+                        if (burgerCheck(j))  {
+                            // System.out.println(j);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    static boolean burgerCheck(int idx) {
+        if (arr[idx] == 'H') {
+            arr[idx] = 'X';
+            cnt++;
+            return true;
+        }
+        return false;
     }
 
 }
--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,4 @@
 public class Test {
 
-	public static void main(String[] args) {
 
-		int i = 0;
-		long sum = 0;
-		while (i<=100) {
-			sum += i;
-			i++;
-		}	
-
-		System.out.println("sum(1~100) : " + sum);
-	}
-	
 }
